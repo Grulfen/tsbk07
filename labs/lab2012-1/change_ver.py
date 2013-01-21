37f3d7245b641e285fe9cefbf37dcbb324f185b0
Grulfen <grulfen3@gmail.com>
2013-01-21 10:42:30 +0100

    Fixed the rule line in the makefile

--- labs/lab2012-1/change_ver.py
+++ labs/lab2012-1/change_ver.py
@@ -3,20 +3,22 @@ import shutil
 import os
 import re
 
+
 def check_and_format_args(argv):
     if len(argv) != 3:
         print "Usage: python change_ver.py lab_number new_lab_part"
         sys.exit(1)
     else:
-        return int(argv[1]), int (argv[2])
+        return int(argv[1]), int(argv[2])
 
 
 def copy_files(lab_no, lab_part):
     extentions = ["c", "frag", "vert"]
     for ext in extentions:
-        infile = "lab{0}-{1}".format(lab_no, lab_part-1)
+        infile = "lab{0}-{1}".format(lab_no, lab_part - 1)
         outfile = "lab{0}-{1}".format(lab_no, lab_part)
         copy_file(infile, outfile, ext)
+
 
 def copy_file(src, dst, ext):
     src_full = src + "." + ext
@@ -25,13 +27,14 @@ def copy_file(src, dst, ext):
         print "{0} already exists".format(dst_full)
         return
     try:
-        src_file = open( src_full, 'r')
+        src_file = open(src_full, 'r')
     except IOError:
         print "Could not open {0}".format(src_full)
+        sys.exit(1)
         return
 
     try:
-        dst_file = open( dst_full, 'w')
+        dst_file = open(dst_full, 'w')
     except IOError:
         print("Error: Could not write to file {0}".format(dst_full))
 
@@ -46,6 +49,7 @@ def copy_file(src, dst, ext):
             dst_file.write(line)
     src_file.close()
     dst_file.close()
+
 
 def edit_makefile(lab_no, lab_part):
     print "Editing makefile"
@@ -68,7 +72,7 @@ def edit_makefile(lab_no, lab_part):
                 already_made = True
                 out_make.write(line)
             else:
-                out_make.write(line[:-1] + ' ' +  this_lab + '\n')
+                out_make.write(line[:-1] + ' ' + this_lab + '\n')
 
         elif line.startswith('lab'):
             out_make.write(line)
@@ -77,8 +81,7 @@ def edit_makefile(lab_no, lab_part):
 
         elif line.startswith('clean'):
             out_make.write("{0}: {0}.c GL_utilities.c\n".format(this_lab))
-            print rule
-            rule_line = re.sub(r'lab[0-9]-[0-9]', this_lab, rule)
+            rule = re.sub(r'lab[0-9]-[0-9]', this_lab, rule)
             out_make.write(rule + '\n')
             out_make.write(line)
 
